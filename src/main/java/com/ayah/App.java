8dff8ba3db1037d57ea884eaa7914a5acaa7875b
AyahRefai <AyahRefai@arefai>
2023-01-11 13:17:06 +0300

    Third commit by Ayah Alrifai 1

--- src/main/java/com/ayah/App.java
+++ src/main/java/com/ayah/App.java
@@ -3,13 +3,17 @@ package com.ayah;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+	public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext app_context = new ClassPathXmlApplicationContext();
 
-        Reading reading = (Reading) context.getBean("reading", Reading.class);
-       
-        reading.getReader().read();
+		System.out.println("start");
 
-        context.close();
-    }
+        Reading reading = (Reading) app_context.getBean("reading", Reading.class);
+
+		reading.getReader().read();
+
+		System.out.println("end");
+
+        app_context.close();
+	}
 }
